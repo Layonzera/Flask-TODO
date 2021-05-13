fb38b46b0d5081a4477320e45fe58b7612de6cf0
Adryann Layon <adryann.layon10@hotmail.com>
2021-05-13 23:56:05 +0000

    Added flash messages

--- main.py
+++ main.py
@@ -4,54 +4,54 @@ from controllers import TodoController, UserController
 from database import db
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '133da5ef683757d14d5839882ec18807'
+app.config['SECRET_KEY'] = '05c31152d33d28330bfd6cdfb5ee36e2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 db.init_app(app)
 
 @app.route('/')
 def index():
-    return TodoController.index()
+  return TodoController.index()
 
 @app.route('/login')
 def login():
-    return UserController.login()
+  return UserController.login()
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    return UserController.signin()
-    
+  return UserController.signin()
+
 @app.route('/register')
 def register():
-    return UserController.register()
- 
+  return UserController.register()
+
 @app.route('/signup', methods=['POST'])
 def signup():
-    return UserController.signup()
+  return UserController.signup()
 
 @app.route('/logout')
 def logout():
-    return UserController.logout()
+  return UserController.logout()
 
 @app.route('/create', methods=['POST'])
 def create():
-    return TodoController.create()
+  return TodoController.create()
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    return TodoController.delete(id)
+  return TodoController.delete(id)
 
 @app.route('/complete/<int:id>')
 def complete(id):
-    return TodoController.complete(id)
+  return TodoController.complete(id)
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update(id):
-    return TodoController.update(id)
+  return TodoController.update(id)
 
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0', port = port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host = '0.0.0.0', port = port)
